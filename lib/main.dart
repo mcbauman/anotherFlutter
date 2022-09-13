6fd2efa326d3dc0b50663db27c249dd250f09956
mcbauman <m.c.baumann@icloud.com>
2022-09-13 18:01:08 +0200

    state management solve finally

--- lib/main.dart
+++ lib/main.dart
@@ -6,23 +6,15 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 String token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI2MmY0MDEzY2I3ZmNiMzg2NjQ4ZGE1NWMiLCJpYXQiOjE2NjIxMDAyODEsImV4cCI6MTY2MjE4NjY4MX0.0-WO-hHx4_F1YdfTAzp5x_0Gw7QmuYKiPriqpnDzfYM";
-// int user = 2;
-var any = MyModel();
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI2MmY0MDEzY2I3ZmNiMzg2NjQ4ZGE1NWMiLCJpYXQiOjE2NjMwNjE0ODYsImV4cCI6MTY2MzE0Nzg4Nn0.zo3bHi_pBANsWBSGfNo3fB5gwsp1JbfQS6X5wV19OzY";
 
 void main() {
-  runApp(const MyApp2());
+  runApp(const MyApp());
 }
 
-class MyApp2 extends StatefulWidget {
-  const MyApp2({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp2> createState() => _MyApp2State();
-}
-
-class _MyApp2State extends State<MyApp2> {
-  //int user = 2;
   @override
   Widget build(BuildContext context) {
     Future<List<ItemClass>> getRequest() async {
@@ -45,151 +37,127 @@ class _MyApp2State extends State<MyApp2> {
       return reqItems;
     }
 
-    return ChangeNotifierProvider<MyModel>(
-        //      <--- ChangeNotifierProvider
-        create: (context) => MyModel(),
-        child: MaterialApp(
-            title: 'Welcome to Flutter',
-            // themeMode: ThemeMode.system,
-            // theme: MyThemes.lightTheme,
-            // darkTheme: MyThemes.darkTheme,
-            home: Stack(children: [
-              Container(
-                  decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage("assets/img1.jpeg"),
-                          fit: BoxFit.cover))),
-              Scaffold(
-                backgroundColor: Colors.transparent,
-                appBar: AppBar(
-                    flexibleSpace: ClipRRect(
-                        child: BackdropFilter(
-                            filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-                            child: Container())),
-                    elevation: 0,
-                    leading: const IconButton(
-                        onPressed: null, icon: Icon(Icons.dark_mode)),
-                    title: Consumer<MyModel>(//                    <--- Consumer
-                        builder: (context, myModel, child) {
-                      return Text(
-                        any.user == "0" ? "LogIn" : "${any.user}'s ToDoList",
-                        style: const TextStyle(
-                          color: Color.fromARGB(255, 0, 0, 0),
-                        ),
-                      );
-                    }),
-                    actions: [
-                      TextButton(
-                          onPressed: () => {
-                                setState(() {
-                                  any.setUser("0");
-                                })
-
-                                // setState(() {
-                                //   any.user = "0";
-                                // }),
-                                // print(any.user)
-                              },
-                          child: Text(
-                              style:
-                                  TextStyle(fontSize: 20, color: Colors.black),
-                              "Logout")),
-                    ],
-                    backgroundColor: const Color.fromARGB(100, 255, 255, 255)),
-                body: Consumer<MyModel>(//                    <--- Consumer
-                    builder: (context, myModel, child) {
-                  return any.user == "0"
-                      ? Login()
-                      : Container(
-                          margin: const EdgeInsets.fromLTRB(0, 100, 0, 0),
-                          child: FutureBuilder(
-                              future: getRequest(),
-                              builder:
-                                  (BuildContext ctx, AsyncSnapshot snapshot) {
-                                if (snapshot.data == null) {
-                                  return const Center(
-                                    child: CircularProgressIndicator(),
-                                  );
-                                } else {
-                                  return ListView.builder(
-                                      itemCount: snapshot.data.length,
-                                      itemBuilder: (ctx, index) =>
-                                          ListItem(item: snapshot.data[index]));
-                                }
-                              }));
-                }),
-                floatingActionButton: FloatingActionButton(
-                    tooltip: 'Add',
-                    onPressed: null,
-                    backgroundColor: Colors.transparent,
-                    elevation: 0,
-                    child: ClipRRect(
-                        child: BackdropFilter(
-                            filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  borderRadius: const BorderRadius.all(
-                                      Radius.circular(10.0)),
-                                  border: Border.all(
-                                      color: const Color.fromARGB(
-                                          180, 255, 255, 255),
-                                      width: 1),
-                                  color:
-                                      const Color.fromARGB(45, 255, 255, 255)),
-                              child: const Icon(Icons.add),
-                            )))),
-              )
-            ])));
+    return ChangeNotifierProvider(
+        create: (context) => SettingsScreenNotifier(),
+        builder: (context, provider) {
+          return Consumer<SettingsScreenNotifier>(
+            builder: (context, notifier, child) {
+              return MaterialApp(
+                  title: 'Welcome to Flutter',
+                  home: Stack(children: [
+                    Container(
+                        decoration: const BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage("assets/img1.jpeg"),
+                                fit: BoxFit.cover))),
+                    Scaffold(
+                      backgroundColor: Colors.transparent,
+                      appBar: AppBar(
+                          flexibleSpace: ClipRRect(
+                              child: BackdropFilter(
+                                  filter:
+                                      ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                                  child: Container())),
+                          elevation: 0,
+                          leading: const IconButton(
+                              onPressed: null, icon: Icon(Icons.dark_mode)),
+                          title: Text(
+                            notifier.currentUSer == "0"
+                                ? "LogIn"
+                                : "${notifier.currentUSer}'s ToDoList",
+                            style: const TextStyle(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                            ),
+                          ),
+                          actions: [
+                            TextButton(
+                                onPressed: () => {notifier.switchUser("0")},
+                                child: const Text(
+                                  "Logout",
+                                )),
+                          ],
+                          backgroundColor:
+                              const Color.fromARGB(100, 255, 255, 255)),
+                      body: notifier.currentUSer == "0"
+                          ? const Login()
+                          : Container(
+                              margin: const EdgeInsets.fromLTRB(0, 100, 0, 0),
+                              child: FutureBuilder(
+                                  future: getRequest(),
+                                  builder: (BuildContext ctx,
+                                      AsyncSnapshot snapshot) {
+                                    if (snapshot.data == null) {
+                                      return const Center(
+                                        child: CircularProgressIndicator(),
+                                      );
+                                    } else {
+                                      return ListView.builder(
+                                          itemCount: snapshot.data.length,
+                                          itemBuilder: (ctx, index) => ListItem(
+                                              item: snapshot.data[index]));
+                                    }
+                                  })),
+                      floatingActionButton: FloatingActionButton(
+                          tooltip: 'Add',
+                          onPressed: null,
+                          backgroundColor: Colors.transparent,
+                          elevation: 0,
+                          child: ClipRRect(
+                              child: BackdropFilter(
+                                  filter:
+                                      ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                        borderRadius: const BorderRadius.all(
+                                            Radius.circular(10.0)),
+                                        border: Border.all(
+                                            color: const Color.fromARGB(
+                                                180, 255, 255, 255),
+                                            width: 1),
+                                        color: const Color.fromARGB(
+                                            45, 255, 255, 255)),
+                                    child: const Icon(Icons.add),
+                                  )))),
+                    )
+                  ]));
+            },
+          );
+        });
   }
 }
 
-class Login extends StatefulWidget {
-  Login({Key? key}) : super(key: key);
-  @override
-  State<Login> createState() => _LoginState();
-}
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
 
-class _LoginState extends State<Login> {
-  final _formKey = GlobalKey<FormState>();
+  //   var api = Uri.http("todolistmcb.herokuapp.com", "getItems");
+  // var response = await http.post(api, headers: {
+  //   "Content-Type": "application/json",
+  //   "Authorization": "Bearer $token"
+  // });
+  // print('Response body: ${response.body}');
+  // var responseData = json.decode(response.body);
+
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<MyModel>(
-        //      <--- ChangeNotifierProvider
-        create: (context) => MyModel(),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              Text(any.user),
-              TextFormField(
-                  decoration:
-                      const InputDecoration(hintText: 'Enter your name')),
-              TextFormField(
-                  decoration:
-                      const InputDecoration(hintText: 'Enter your password')),
-              Consumer<MyModel>(builder: (context, myModel, child) {
-                return IconButton(
-                    onPressed: () => {
-                          setState(() {
-                            any.setUser("2");
-                          })
-                        },
-
-                    // onPressed: () => {
-                    //   // print("Login-Submit-Pressed")
-                    //   setState() {any.setUser("0");}
-
-                    //   // setState(() {
-                    //   //   any.user = "2";
-                    //   // });
-                    //   // print("User in LoginClass ${any.user}");
-                    // },
-                    icon: const Icon(Icons.login));
-              })
-              // Add TextFormFields and ElevatedButton here.
-            ],
-          ),
-        ));
+    return Consumer<SettingsScreenNotifier>(
+        builder: (context, notifier, child) {
+      return Form(
+        child: Column(
+          children: <Widget>[
+            Text(notifier.currentUSer),
+            TextFormField(
+                decoration: const InputDecoration(hintText: 'Enter your name')),
+            TextFormField(
+                decoration:
+                    const InputDecoration(hintText: 'Enter your password')),
+            IconButton(
+                onPressed: () => {notifier.switchUser("2")},
+                icon: const Icon(Icons.login))
+          ],
+        ),
+      );
+    });
+    //});
   }
 }
 
@@ -247,27 +215,11 @@ class _ListItemState extends State<ListItem> {
   }
 }
 
-class MyModel with ChangeNotifier {
-  String user = '2';
-
-  void setUser(String newUser) {
-    user = newUser;
-    print("SetUSerFUnction in MyModel $user");
+class SettingsScreenNotifier extends ChangeNotifier {
+  String _currentUser = "2";
+  get currentUSer => _currentUser;
+  void switchUser(String newUser) {
+    _currentUser = newUser;
     notifyListeners();
   }
 }
-
-
-// class Context extends StatefulWidget with ChangeNotifier {
-//   String user = "0";
-
-//   @override
-//   State<Context> createState() => _ContextState();
-// }
-
-// class _ContextState extends State<Context> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return 
-//   }
-// }
